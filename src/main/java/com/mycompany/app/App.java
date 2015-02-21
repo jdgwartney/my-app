cb17e0470eb64e41f8b2d88182bc23dbd6ea74dd
jdgwartney <davidg@boundary.com>
2015-02-20 23:43:31 -0800

    Add JSON configuration file

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,15 +1,46 @@
 package com.mycompany.app;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 /**
  * Hello world!
  *
  */
 public class App {
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException, URISyntaxException {
 		long count = 1;
+		Configuration configuration = load("META-INF/json/configuration.json");
 		while(true) {
 			System.out.printf("Hello World!-%s%n",count++);
-			Thread.sleep(5000);
+			Thread.sleep(configuration.getSleepDelay());
 		}
+	}
+	
+	public static Configuration load(String resource) throws URISyntaxException {
+		Configuration instance = new Configuration();
+
+		ClassLoader classLoader = instance.getClass().getClassLoader();
+		URL url = classLoader.getResource(resource);
+		File file = new File(url.toURI());
+
+		ObjectMapper mapper = new ObjectMapper();
+
+		try {
+			instance = mapper.readValue(file,Configuration.class);
+		} catch (JsonParseException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return instance;
 	}
 }
