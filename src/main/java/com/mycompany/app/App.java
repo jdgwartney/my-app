e6321addd91dd7d47b83ecc018e3555efba09ac5
jdgwartney <davidg@boundary.com>
2015-02-20 23:19:16 -0800

    Checkpoint

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,10 +4,12 @@ package com.mycompany.app;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) throws InterruptedException {
+		long count = 1;
+		while(true) {
+			System.out.printf("Hello World!-%s%n",count++);
+			Thread.sleep(5000);
+		}
+	}
 }
